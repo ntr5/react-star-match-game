0ed6aa1f362c6c5572eb321ac795b8a50765eec0
pingt9*t <pressntr@gmail.com>
2020-04-13 18:29:38 -0600

    recreated star match game from jsplayground in a react local app and split the functions into separate components

---
+++ src/components/StarsDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import utils from '../math-utils'
+
+const StarsDisplay = props => (
+    <div>
+      {utils.range(1, props.count).map(starId => (
+        <div key={starId} className="star" />
+      ))}
+    </div>
+  );
+
+export default StarsDisplay
